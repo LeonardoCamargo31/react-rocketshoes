ae2cff6d97d70b71cef97a3418521d4efdfaf27c
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2020-07-21 18:48:08 -0300

    stylizing home and cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,66 @@
 import React from 'react'
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md'
+import { Container, ProductTable, Total } from './styles'
 
 export default function Cart() {
-  return <div />
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/12/NQQ-0144-012/NQQ-0144-012_zoom1.jpg"
+                alt=""
+              />
+            </td>
+            <td>
+              <strong>Nome do tênis</strong>
+              <span>R$ 199,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$199,99</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$ 199,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,94 @@
 import React from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles'
 
 export default function Home() {
-  return <div />
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/12/NQQ-0144-012/NQQ-0144-012_zoom1.jpg"
+          alt=""
+        />
+        <strong>Nome</strong>
+        <span>R$ 129,90</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/12/NQQ-0144-012/NQQ-0144-012_zoom1.jpg"
+          alt=""
+        />
+        <strong>Nome</strong>
+        <span>R$ 129,90</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/12/NQQ-0144-012/NQQ-0144-012_zoom1.jpg"
+          alt=""
+        />
+        <strong>Nome</strong>
+        <span>R$ 129,90</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/12/NQQ-0144-012/NQQ-0144-012_zoom1.jpg"
+          alt=""
+        />
+        <strong>Nome</strong>
+        <span>R$ 129,90</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/12/NQQ-0144-012/NQQ-0144-012_zoom1.jpg"
+          alt=""
+        />
+        <strong>Nome</strong>
+        <span>R$ 129,90</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-20-masculino/12/NQQ-0144-012/NQQ-0144-012_zoom1.jpg"
+          alt=""
+        />
+        <strong>Nome</strong>
+        <span>R$ 129,90</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  )
 }
